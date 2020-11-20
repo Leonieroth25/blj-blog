b4d7e504be9f0ecec3b068920a94d67846f7b003
Leonie Roth <leonieroth@Leonies-MacBook-Pro.local>
2020-11-20 14:01:30 +0100

    intal commit.

---
+++ blog.php
@@ -0,0 +1,61 @@
+<?php
+$name    = $_POST['name']    ?? '';
+$contribution   = $_POST['contribution']   ?? '';
+
+if ($name === '') {
+    $errors[] = 'Bitte geben Sie einen Namen ein.';
+}
+if ($contribution === '') {
+    $errors[] = 'Bitte geben Sie einen Text ein.';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leonie's Blog</title>
+    <link rel="stylesheet" href="blog.css">
+</head>
+
+
+<body>
+    <h1>Leonie's Blog</h1>
+   
+    <?php include("nav.php");?>
+
+
+    <p>Dies ist mein erster Blog Beitrag. Ich hoffe er gefällt euch.
+        <br>Unten könnt ihr eure eigenen Beiträge schrieben oder die anderen kommentieren/bewerten.
+        <br>Ich freue mich auf eure Beiträge. <br>Liebe Grüsse Leonie</p>
+
+
+        <?php if(count($errors) > 0) : ?>
+            <div class="error-box">
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+            </div>
+        <?php endif; ?>
+        <form action="blog.php" method="post">
+        <legend class="form-legend">Ihr eigener Beitrag:<br><br></legend>
+                <div class="form-group">
+                    <label class="form-label" for="name">Ihr Name<br></label>
+                    <input class="form-control" type="text" id="name" name="name" value="<?= $name ?? '' ?>">
+                </div>
+
+                <div class="form-group">
+                    <label for="note" class="form-label">Was möchten Sie uns Mitteilen?<br></label>
+                    <textarea name="note" id="contribution" rows="3" class="contribution"><?= $contribution?></textarea>
+                </div>
+
+            <div class="form-actions">
+                <input class="btn btn-primary" type="submit" value="Beitrag posten">
+                <a href="blog.php" class="btn">Beitrag abbrechen</a>
+            </div>
+
+</body>
+</html>
---
+++ kollegen.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Andere Blogs</title>
+    <link rel="stylesheet" href="blog.css">
+</head>
+
+<body>
+<h1>Andere Blogs</h1>
+
+<?php include("nav.php");?>
+
+   
+<a href="">Noah</a><br>
+<a href="">Dawid</a><br>
+<a href="">Natascha</a><br>
+<a href="">Kris</a><br>
+<a href="">Lian</a><br>
+<a href="">Silas</a><br>
+<a href="">Tamara</a><br>
+<a href="">Gian</a><br>
+<a href="">Thierry</a><br>
+<a href="">Rouven</a><br>
+<a href="">Alejandro</a><br>
+<a href="">Dominic</a><br>
+<a href="">Janik</a><br>
+<a href="">Corinne</a><br>
+<a href="">Damiano</a><br>
+
+
+</body>
+</html>
