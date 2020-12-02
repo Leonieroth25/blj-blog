bc5e6ec740be0c2931b9361286017ca5826cc36e
Leonie Roth <leonieroth@Leonies-MBP.local>
2020-12-02 14:15:20 +0100

    comment part 1

--- blog.php
+++ blog.php
@@ -4,6 +4,9 @@ $name = '';
 $contribution = '';
 $date = '';
 $title ='';
+$picture ='';
+$rating = '';
+$comment = '';
 
 $pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=d041e_leroth', 'd041e_leroth', '12345_Db!!!', [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -15,6 +18,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $contribution   = $_POST['contribution']   ?? '';
     $date    = date('y.m.d H:i:s'); 
     $title    = $_POST['title']    ?? '';
+    $picture = $_POST ['picture'] ??'';
+    $rating    = $_POST['rating']    ?? '';
+    $comment    = $_POST['comment']    ?? '';
 
     if (empty($name)) {
         $errors[] = 'Bitte geben Sie einen Namen ein.';
@@ -27,13 +33,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
     
     if (count($errors) == 0) {
-        $stmt = $pdo->prepare("INSERT INTO posts (created_at, created_by, post_title, post_text) VALUES(:post_date, :creator, :title, :post)");
-        $stmt->execute([':post_date' => $date ,':creator' => $name,':title' => $title, ':post' => $contribution]);
+        
+        $stmt = $pdo->prepare("INSERT INTO posts (created_at, created_by, post_title, post_text, comment) VALUES(:post_date, :creator, :title, :post, :comment)");
+        //$stmt = $pdo->prepare("INSERT INTO posts (comment) VALUES(:comment)");
+        $stmt->execute([':post_date' => $date ,':creator' => $name,':title' => $title, ':post' => $contribution, ':comment' => $comment]);
+       
+        
     }
 }
 
 //if (($_GET['action']?? '') === 'all') {
 $query= 'select * from posts order by created_at desc';
+
 //}
 //$query = 'select * from posts';
 
@@ -74,10 +85,46 @@ $rows = $stmt -> fetchAll();
 
     <?php foreach($rows as $rows) {
             echo '<div class="frame">';
-            echo 'Name: ' . htmlspecialchars($rows ["created_by"]) .' / ' . htmlspecialchars($rows ["created_at"]) .'<br>' . 'Titel: ' . htmlspecialchars($rows["post_title"]) . '<br>' . 'Post: ' .  htmlspecialchars($rows["post_text"]) .'<br>';
-                    
+            echo 'Name: ' . htmlspecialchars($rows ["created_by"]) 
+            .' / ' . htmlspecialchars($rows ["created_at"]) .'<br>' 
+            . 'Titel: ' . htmlspecialchars($rows["post_title"]) . '<br>' 
+            . 'Post: ' . htmlspecialchars($rows["post_text"]) .'<br>' 
+            .'Kommentar: ' . htmlspecialchars($rows ["comment"]) 
+            . '<img id="picture" src="' .htmlspecialchars($rows["picture"]) . '" alt="">';
+
+
+            
+            //echo 'Kommentar: ' . htmlspecialchars($rows ["comment"]);
+    ?>
+
+                <br>
+                <div class="form-group">
+                    <label for="comment" class="comment">haha<br></label>
+                    <textarea name="comment" id="comment" rows="3" class="comment"><?= $comment ?></textarea>
+                
+                </div>
+            <?php
+             
+            
+            ?>
+               
+                <div class="form-button">
+                    <input class="btn btn-primary" type="submit" value="Kommentar posten" name="post-btn">
+                    <a href="blog.php" class="btn">Kommentar abbrechen</a>
+                </div>
+
+    <?php
+            
+            
+            
             //echo '<div><a href="blog.php" class="btn">gut</a>/<a href="blog.php" class="btn">schlecht</a></div>';
+
+
+
             echo '</div><br>';
+
+            
+           // echo'<imgid="picture" scr="' .htmlspecialchars($rows["imageurl"]).'"alt="">';
             
         }
         ?>
@@ -85,7 +132,7 @@ $rows = $stmt -> fetchAll();
         <?php
         if(count($errors) > 0) : 
         ?>
-        
+    
             <div class="error-box"> 
             <ul>
                 <?php foreach ($errors as $error) : ?>
@@ -116,7 +163,14 @@ $rows = $stmt -> fetchAll();
                 <textarea name="contribution" id="contribution" rows="5" class="contribution"><?= $contribution ?></textarea>
             </div>
 
+            <div class="form-group">
+                <label class="form-label" for="picture">Link für ein Bild<br></label>
+                <input class="form-control" type="text" id="picture" name="picture" value="<?= $picture ?>">
+                
+            </div>
+        
             <div class="form-actions">
+            <br>
                 <input class="btn btn-primary" type="submit" value="Beitrag posten" name="post-btn">
                 <a href="blog.php" class="btn">Beitrag abbrechen</a>
             </div>
